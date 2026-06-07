c0cf44afa67236eaa9034b787f372fa42bfeb381
Kyrre Martila <kyrre@martila.no>
2026-06-07 14:53:09 +0200

    Fix navigation casing conflict for Windows

--- apps/web/components/Navigation.tsx
+++ apps/web/components/Navigation.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useId, useState } from "react";
-import { bottomNavigationItems, getCreateOptions } from "./navigation";
+import { bottomNavigationItems, getCreateOptions } from "./navigation-options";
 
 type BottomNavigationItem = (typeof bottomNavigationItems)[number];
 
