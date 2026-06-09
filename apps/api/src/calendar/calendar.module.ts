c920e19b45337e441a77982ceb3555729973c413
kyrre-martila <post@weblia.no>
2026-06-09 14:38:27 +0200

    Add AuthModule to CalendarModule imports

--- apps/api/src/calendar/calendar.module.ts
+++ apps/api/src/calendar/calendar.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module";
 import { FamiliesModule } from "../families";
 import { PrismaModule } from "../prisma";
 import { CalendarController } from "./calendar.controller";
 import { CalendarService } from "./calendar.service";
 
 @Module({
-  imports: [PrismaModule, FamiliesModule],
+  imports: [PrismaModule, FamiliesModule, AuthModule],
   controllers: [CalendarController],
   providers: [CalendarService],
   exports: [CalendarService]
